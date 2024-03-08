2c9e2675f4c79fa155cc5c04027b985afd39445a
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-03-08 21:18:41 +0530

    update 1

--- scripts.js
+++ scripts.js
@@ -5,8 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const imageUrls = [
         "./1.jpg",
         "./2.jpg",
-        "./3.jpg",
-        "./4.jpg",
         "./5.jpg",
         "./6.jpg",
         "./7.jpg",
